2dbd394dc83e10282f77ddebb5a9f97cfa2ee86c
Rishabh Poddar <rishabh.poddar@gmail.com>
2020-04-08 14:38:04 +0530

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,123 @@
-# supertokens-docker-mongodb
-Docker image for SuperTokens Community and MongoDB
+## About this image
+
+The following parameters are required to start the container:
+- `dev` / `production` mode
+- `config.yaml` environment variables (Configuration)
+- License Key file or License Key ID
+
+
+## `dev` / `production` mode
+- The default value of this is `dev`
+- This is the same as what the CLI expects. You can learn more about the differences [here](https://supertokens.io/docs/pro/cli/start)
+
+
+## Configuration
+You can use your own `config.yaml` file as a shared volume or pass the key-values as environment variables. 
+
+If you do both, only the shared `config.yaml` file will be considered.
+  
+#### Using environment variable
+Available environment variables
+- **Core** [[click for more info](https://supertokens.io/docs/pro/configuration/core)]
+	- COOKIE\_DOMAIN \[**required**\]
+	- REFRESH\_API\_PATH \[**required**\]
+	- SUPERTOKENS\_HOST
+	- SUPERTOKENS\_PORT
+	- ACCESS\_TOKEN\_VALIDITY
+	- ACCESS\_TOKEN\_BLACKLISTING
+	- ACCESS\_TOKEN\_PATH
+	- ACCESS\_TOKEN\_SIGNING\_KEY\_DYNAMIC
+	- ACCESS\_TOKEN\_SIGNING\_KEY\_UPDATE\_INTERVAL
+	- ENABLE\_ANTI\_CSRF
+	- REFRESH\_TOKEN\_VALIDITY
+	- INFO\_LOG\_PATH
+	- ERROR\_LOG\_PATH
+	- COOKIE\_SECURE
+    - MAX\_SERVER\_POOL\_SIZE
+- **MongoDB:** [[click for more info](https://supertokens.io/docs/pro/configuration/database/mongodb)]	
+	- MONGODB\_CONNECTION\_URI \[**required**\]
+	- MONGODB\_DATABASE\_NAME
+	- MONGODB\_KEY\_VALUE\_COLLECTION\_NAME
+	- MONGODB\_SESSION\_INFO\_COLLECTION\_NAME
+	- MONGODB\_PAST\_TOKENS\_COLLECTION\_NAME
+- **License Key**: [See below]
+	- LICENSE_KEY_ID
+  
+
+```bash
+$ docker run 
+	-e MONGODB_CONNECTION_URI=mongodb://root:root@localhost:27017 \
+	-e COOKIE_DOMAIN=example.com \
+	-e REFRESH_API_PATH=/example/refresh \
+	-e LICENSE_KEY_ID=yourLicenseKeyID \
+	-d supertokens-mongodb-pro dev
+```
+
+#### Using custom config file
+- In your `config.yaml` file, please make sure you store the following key / values:
+  - `core_config_version: 0`
+  - `host: "0.0.0.0"`
+  - `mongodb_config_version: 0`
+  - `info_log_path: null` (to log in docker logs)
+  - `error_log_path: null` (to log in docker logs)
+- The path for the `config.yaml` file in the container is `/usr/lib/supertokens/config.yaml`
+
+```bash
+$ docker run \
+	-v /path/to/config.yaml:/usr/lib/supertokens/config.yaml \
+	-e LICENSE_KEY_ID=yourLicenseKeyID \
+	-d supertokens-mongodb-pro dev
+```
+
+## License Key
+You can get your license key from your [SuperTokens dashboard](https://supertokens.io/dashboard).
+
+
+You can either share your `licenseKey` file, or provide the ID as an environment variable. We recommend providing the file since that way you can run the container without giving it internet access.
+
+Please check this [link](https://supertokens.io/docs/pro/about-license-keys) to learn more about license keys.
+
+#### Using environment variables
+```bash
+$ docker run \
+	-e MONGODB_CONNECTION_URI=mongodb://root:root@localhost:27017 \
+	-e COOKIE_DOMAIN=example.com \
+	-e REFRESH_API_PATH=/example/path \
+	-e LICENSE_KEY_ID=<your-license-key-id> \
+	-d supertokens-mongodb-pro production
+```
+
+#### Using your `licenseKey` file
+```bash
+$ docker run \
+	-e MONGODB_CONNECTION_URI=mongodb://root:root@localhost:27017 \
+	-e COOKIE_DOMAIN=example.com \
+	-e REFRESH_API_PATH=/example/path \
+	-v /path/to/licenseKey:/usr/lib/supertokens/licenseKey \	
+	-d supertokens-mongodb-pro dev
+```
+
+## Logging
+- By default, all the logs will be available via the `docker logs <container-name>` command.
+- You can setup logging to a shared volume by:
+	- Setting the `info_log_path` and `error_log_path` variables in your `config.yaml` file (or passing the values asn env variables).
+	- Mounting the shared volume for the logging directory.
+
+```bash
+$ docker run \
+	-v /path/to/logsFolder:/home/logsFolder \
+	-e INFO_LOG_PATH=/home/logsFolder/info.log \
+	-e ERROR_LOG_PATH=/home/logsFolder/error.log \
+	-e MONGODB_CONNECTION_URI=mongodb://root:root@localhost:27017 \
+	-e COOKIE_DOMAIN=example.com \
+	-e REFRESH_API_PATH=/example/path \
+	-e LICENSE_KEY_ID=yourLicenseKeyId \
+	-d supertokens-mongodb-pro production
+```
+
+## Database setup
+- You do not need to ensure that the MongoDB database has started before this container is started. During bootup, SuperTokens will wait for ~1 hour for a MongoDB instance to be available.
+
+
+## CLI reference
+Please refer to our [documentation](https://supertokens.io/docs/pro/cli/overview) for this.
---
+++ test.sh
@@ -0,0 +1,168 @@
+set -e
+# build image
+docker build -t supertokens-mongodb:circleci .
+
+test_equal () {
+    if [[ $1 -ne $2 ]]
+    then
+        printf "\x1b[1;31merror\x1b[0m in $3\n"
+        exit 1
+    fi
+}
+
+no_of_running_containers () {
+    docker ps -q | wc -l
+}
+
+test_hello () {
+    message=$1
+    STATUS_CODE=$(curl -I -X GET http://127.0.0.1:3567/hello -o /dev/null -w '%{http_code}\n' -s)
+    if [[ $STATUS_CODE -ne "200" ]]
+    then
+        printf "\x1b[1;31merror\xd1b[0m in $message\n"
+        exit 1
+    fi
+}
+
+test_session_post () {
+    message=$1
+    STATUS_CODE=$(curl -X POST http://127.0.0.1:3567/session -H "Content-Type: application/json" -d '{
+        "userId": "testing",
+        "userDataInJWT": {},
+        "userDataInDatabase": {},
+        "deviceDriverInfo": {
+            "frontendSDK": [{
+                "name": "ios",
+                "version": "1.0.0"
+            }],
+            "driver": {
+                "name": "node",
+                "version": "1.0.0"
+            }
+        }
+    }' -o /dev/null -w '%{http_code}\n' -s)
+    if [[ $STATUS_CODE -ne "200" ]]
+    then
+        printf "\x1b[1;31merror\xd1b[0m in $message\n"
+        exit 1
+    fi
+}
+
+LICENSE_FILE_PATH=$PWD/licenseKey
+curl -X GET "https://api.supertokens.io/development/license-key?password=$API_KEY&planType=FREE&onExpiry=NA&expired=False" -H "api-version: 0" -s > $LICENSE_FILE_PATH
+LICENSE_KEY_ID=$(cat $LICENSE_FILE_PATH | jq -r ".info.licenseKeyId")
+
+# start mongodb server
+docker run --rm -d -p 27017:27017 --name mongodb -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=root mongo
+
+sleep 26s
+
+# setting network options for testing
+OS=`uname`
+
+NETWORK_OPTIONS="-p 3567:3567 -e MONGODB_CONNECTION_URI=mongodb://root:root@$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1):27017"
+printf "\nmongodb_connection_uri: \"mongodb://root:root@$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1):27017\"\n" >> $PWD/config.yaml
+
+#---------------------------------------------------
+# start with mongodb connection_uri, cookie domain and refresh API path
+docker run $NETWORK_OPTIONS -e COOKIE_DOMAIN=supertokens.io -e REFRESH_API_PATH=/auth/refresh --rm -d --name supertokens supertokens-mongodb:circleci
+
+sleep 10s
+
+test_equal `no_of_running_containers` 1 "start with mongodb connection_uri, cookie domain and refresh API path"
+
+#---------------------------------------------------
+# start with mongodb connection_uri, license key id and refresh API path
+docker run $NETWORK_OPTIONS -e REFRESH_API_PATH=/auth/refresh -e LICENSE_KEY_ID=$LICENSE_KEY_ID --rm -d --name supertokens supertokens-mongodb:circleci
+
+sleep 10s
+
+test_equal `no_of_running_containers` 1 "start with mongodb connection_uri, license key id and refresh API path"
+
+#---------------------------------------------------
+# start with mongodb connection_uri, cookie domain and license key id
+docker run $NETWORK_OPTIONS -e COOKIE_DOMAIN=supertokens.io -e LICENSE_KEY_ID=$LICENSE_KEY_ID --rm -d --name supertokens supertokens-mongodb:circleci
+
+sleep 10s
+
+test_equal `no_of_running_containers` 1 "start with mongodb connection_uri, cookie domain and license key id"
+
+#---------------------------------------------------
+# start with mongodb connection_uri, cookie domain refresh API path and license key id
+docker run $NETWORK_OPTIONS -e COOKIE_DOMAIN=supertokens.io -e REFRESH_API_PATH=/auth/refresh -e LICENSE_KEY_ID=$LICENSE_KEY_ID --rm -d --name supertokens supertokens-mongodb:circleci
+
+sleep 17s
+
+test_equal `no_of_running_containers` 2 "start with mongodb connection_uri, cookie domain refresh API path and license key id"
+
+test_hello "start with mongodb connection_uri, cookie domain refresh API path and license key id"
+
+test_session_post "start with mongodb connection_uri, cookie domain refresh API path and license key id"
+
+docker rm supertokens -f
+
+#---------------------------------------------------
+# start by sharing config.yaml without license key id
+docker run $NETWORK_OPTIONS -v $PWD/config.yaml:/usr/lib/supertokens/config.yaml --rm -d --name supertokens supertokens-mongodb:circleci
+
+sleep 10s
+
+test_equal `no_of_running_containers` 1 "start by sharing config.yaml without license key id"
+
+#---------------------------------------------------
+# start by sharing config.yaml with license key id
+docker run $NETWORK_OPTIONS -v $PWD/config.yaml:/usr/lib/supertokens/config.yaml -e LICENSE_KEY_ID=$LICENSE_KEY_ID --rm -d --name supertokens supertokens-mongodb:circleci
+
+sleep 17s
+
+test_equal `no_of_running_containers` 2 "start by sharing config.yaml with license key id"
+
+test_hello "start by sharing config.yaml with license key id"
+
+test_session_post "start by sharing config.yaml with license key id"
+
+docker rm supertokens -f
+
+#---------------------------------------------------
+# start by sharing config.yaml and license key file
+docker run $NETWORK_OPTIONS -v $PWD/config.yaml:/usr/lib/supertokens/config.yaml -v $LICENSE_FILE_PATH:/usr/lib/supertokens/licenseKey --rm -d --name supertokens supertokens-mongodb:circleci
+
+sleep 17s
+
+test_equal `no_of_running_containers` 2 "start by sharing config.yaml and license key file"
+
+test_hello "start by sharing config.yaml and license key file"
+
+test_session_post "start by sharing config.yaml and license key file"
+
+docker rm supertokens -f
+
+rm -rf $LICENSE_FILE_PATH
+
+# ---------------------------------------------------
+# test info path
+docker run $NETWORK_OPTIONS -v $PWD:/home/supertokens -e COOKIE_DOMAIN=supertokens.io -e INFO_LOG_PATH=/home/supertokens/info.log -e ERROR_LOG_PATH=/home/supertokens/error.log -e REFRESH_API_PATH=/auth/refresh -e LICENSE_KEY_ID=$LICENSE_KEY_ID --rm -d --name supertokens supertokens-mongodb:circleci
+
+sleep 17s
+
+test_equal `no_of_running_containers` 2 "test info path"
+
+test_hello "test info path"
+
+test_session_post "test info path"
+
+if [[ ! -f $PWD/info.log || ! -f $PWD/error.log ]]
+then
+    exit 1
+fi
+
+docker rm supertokens -f
+
+rm -rf $PWD/info.log
+rm -rf $PWD/error.log
+git checkout $PWD/config.yaml
+
+docker rm mongodb -f
+
+printf "\x1b[1;32m%s\x1b[0m\n" "success"
+exit 0
